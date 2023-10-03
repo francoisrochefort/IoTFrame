683bbab78886423e2a1f635cd9f6bc85506fc7f6
author <francoisrochefort@hotmail.fr>
2023-10-03 11:48:27 -0400

    original commit

--- app/src/main/java/com/etrak/scaleusb/di/AppModule.kt
+++ app/src/main/java/com/etrak/scaleusb/di/AppModule.kt
@@ -1,8 +1,8 @@
 package com.etrak.scaleusb.di
 
 import com.etrak.scaleusb.Db
-import com.etrak.scaleusb.Scale
 import com.etrak.scaleusb.domain.customer.CustomerRepository
+import com.etrak.scaleusb.domain.scale.Scale
 
 interface AppModule {
     val db: Db
